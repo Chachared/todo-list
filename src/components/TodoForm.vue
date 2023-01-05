a0b78b0c156ce4dd040b165d544fe190255ee62b
Charlotte Redier <62300614+Chachared@users.noreply.github.com>
2023-01-05 15:35:55 +0100

    mode strict false TS

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -3,7 +3,7 @@
 const props = defineProps<{ content: string }>();
 const emit = defineEmits<{
     (e:'cancel');
-    (e:'update', updatedContent: string)
+     (e:'update', updatedContent: string)
 }>();
 const input = ref<string>(props.content);
 </script>
@@ -12,10 +12,10 @@ const input = ref<string>(props.content);
 <template>
     <div class="d-flex flex-fill">
       <input v-model="input" type="text" class="mx-2 flex-fill" />
-      <button @click="emit('cancel')" class="btn btn-danger mx-2">
+      <button @click="emit('cancel')" class="btn btn-danger mx-2 rounded-pill">
         Annuler
       </button>
-      <button @click="emit('update', input)" class="btn btn-primary">
+      <button @click="emit('update', input)" class="btn btn-primary rounded-pill">
         Sauvegarder
       </button>
     </div>
